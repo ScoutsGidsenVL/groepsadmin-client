42aaf0cbdafda1094aa7b11c00a0b1132b588487
Bram Scheerder <bs@scoutsengidsenvlaanderen.be>
2016-04-08 13:51:20 +0200

    Zoekveld geupdate naar ui.bootstrap 1.3.1

--- app/controllers/search-controller.js
+++ app/controllers/search-controller.js
@@ -7,39 +7,26 @@
 
   SearchController.$inject = ['$scope', '$location'];
 
-  function SearchController($scope, $location/*, searchFactory*/) {
-    var data = [{id: "profiel", voornaam: "Marieke", achternaam: "Vandecasteele", geboortedatum: "05/07/1989"},
-                {id: "profiel", voornaam: "Marie-Sophie", achternaam: "Cnockaert", geboortedatum: "03/10/1990"},
-                {id: "profiel", voornaam: "Marie-Alix", achternaam: "Maelfait", geboortedatum: "29/11/1998"},
-                {id: "profiel", voornaam: "Marie", achternaam: "Asscherickx", geboortedatum: "26/06/1980"},
-                {id: "profiel", voornaam: "Marie-Amelie", achternaam: "Cnockaert", geboortedatum: "08/08/1989"},
-                {id: "profiel", voornaam: "Marieke", achternaam: "Pattijn", geboortedatum: "23/06/2001"},
-                {id: "profiel", voornaam: "Marie", achternaam: "Beel", geboortedatum: "01/03/2004"},
-                {id: "profiel", voornaam: "Marie", achternaam: "Leenknecht", geboortedatum: "01/03/2004"}];
-    $scope.results = [];
+  function SearchController($scope, $location) {
 
-    $scope.zoekLid = function(input) {
+    // Haal leden op via api
+    $scope.zoekLeden = function(zoekterm) {
       // Temporary search
-      $scope.results = jQuery.grep(data, function( n, i ) {
-        return (n.voornaam.toLowerCase().indexOf($scope.input.toLowerCase()) > -1 || n.achternaam.toLowerCase().indexOf($scope.input.toLowerCase()) > -1);
-      });
+      return [{id: "profiel", voornaam: "Marieke", achternaam: "Vandecasteele", geboortedatum: "05/07/1989"},
+              {id: "profiel", voornaam: "Marie-Sophie", achternaam: "Cnockaert", geboortedatum: "03/10/1990"},
+              {id: "profiel", voornaam: "Marie-Alix", achternaam: "Maelfait", geboortedatum: "29/11/1998"},
+              {id: "profiel", voornaam: "Marie", achternaam: "Asscherickx", geboortedatum: "26/06/1980"},
+              {id: "profiel", voornaam: "Marie-Amelie", achternaam: "Cnockaert", geboortedatum: "08/08/1989"},
+              {id: "profiel", voornaam: "Marieke", achternaam: "Pattijn", geboortedatum: "23/06/2001"},
+              {id: "profiel", voornaam: "Marie", achternaam: "Beel", geboortedatum: "01/03/2004"},
+              {id: "profiel", voornaam: "Marie", achternaam: "Leenknecht", geboortedatum: "01/03/2004"}];
     }
 
-    $scope.gotoLid = function(lid) {
+    // ganaar het geselecteerde lid
+    $scope.gaNaarLid = function(lid) {
+      $scope.zoekterm = "";
       $location.path('/lid/' + lid.id);
+
     };
   }
-
-
-  /*
-  .factory('searchFactory', ['$http', function($http) {
-    /*return {
-      get: function(url) {
-        return $http.get(url).then(function(resp) {
-          return resp.data; // success callback returns this
-        });
-      }
-    };
-  }])*/
-
 })();
