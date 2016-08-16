54758f680486f7a61f451b45a10bc9de391a97d8
Bram Scheerder <bs@scoutsengidsenvlaanderen.be>
2016-08-16 15:58:13 +0200

    Algemeen - toon gebruikersnaam wanneer er geen naam ingesteld is.

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -5,13 +5,18 @@
     .module('ga.usercontroller', ['ga.services.alert', 'ga.services.dialog', 'ui.bootstrap'])
     .controller('UserController', UserController);
 
-  UserController.$inject = ['$scope', 'keycloak' ];
+  UserController.$inject = ['$scope', 'keycloak', '$window' ];
 
-  function UserController ($scope, keycloak) {
-    console.log('login = ' + keycloak.authenticated);
-    $scope.username = keycloak.idTokenParsed.name
+  function UserController ($scope, keycloak, $window) {
+    $scope.username = keycloak.idTokenParsed.name;
+    console.log(keycloak.idTokenParsed);
+    if(!keycloak.idTokenParsed.name){
+      $scope.username = keycloak.idTokenParsed.preferred_username;
+    }
     $scope.logout = function(){
+      console.log('logout');
       keycloak.logout();
+      $window.location.href = 'http://login.scoutsengidsenvlaanderenen.be';
     }
 
   }
