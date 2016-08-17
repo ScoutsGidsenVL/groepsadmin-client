c82fba503d968240c74d0f5b60a9ecffaf14479e
Bram Scheerder <bs@scoutsengidsenvlaanderen.be>
2016-08-17 11:22:06 +0200

    Groepsinstellingen - lokalen details en openklappen toegevoegd

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -9,15 +9,12 @@
 
   function UserController ($scope, keycloak, $window) {
     $scope.username = keycloak.idTokenParsed.name;
-    console.log(keycloak.idTokenParsed);
     if(!keycloak.idTokenParsed.name){
       $scope.username = keycloak.idTokenParsed.preferred_username;
     }
     $scope.logout = function(){
-      console.log('logout');
       keycloak.logout();
       $window.location.href = 'http://login.scoutsengidsenvlaanderenen.be';
     }
-
   }
 })();
