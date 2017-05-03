2adfbd70c5ceda78bc1b07e83d82590bf3064042
Johan Degrieck <johand@hakka.eu>
2017-05-03 17:22:55 +0200

    bugfix actieve menu items (ook mobile)

--- app/directives/actieve-link.js
+++ app/directives/actieve-link.js
@@ -12,11 +12,11 @@
               if (element[0].id == 'search') {
                   path = 'zoeken';
               } else {
-                  path = element[0].firstElementChild.hash.substring(1);
+                  path = element[0].firstElementChild.hash;
               }
               scope.location = location;
               scope.$watch('location.path()', function(newPath) {
-                  if (path === newPath) {
+                  if (element[0].firstElementChild.hash.substr(1) === newPath) {
                       element.addClass(clazz);
                   } else {
                       element.removeClass(clazz);
