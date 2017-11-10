0178731c8c0e143b8d8a0807cb158c04185edda4
Johan Degrieck <johand@hakka.eu>
2017-11-10 12:17:10 +0100

    performance menu permissions calls

--- app/controllers/menu-controller.js
+++ app/controllers/menu-controller.js
@@ -8,52 +8,51 @@
   MenuController.$inject = ['$scope', '$window', 'UserAccess'  ];
 
   function MenuController ($scope, $window, UserAccess) {
+    UserAccess.hasAccessToGroepen().then(function(condition){
+      $scope.menuItems = [
+        {
+          label: 'Ledenlijst',
+          condition: UserAccess.hasAccessTo("ledenlijst"),
+          iconclasses : 'fa fa-users',
+          href: '#/ledenlijst'
+        },
+        {
+          label: 'Ledenaantallen',
+          condition: condition,
+          iconclasses : 'fa fa-area-chart',
+          href: '#/orakel'
+        },
+        {
+          label: 'Groepsinstellingen',
+          condition: condition,
+          iconclasses : 'fa fa-cogs',
+          href: '#/groepsinstellingen'
+        },
+        {
+          label: 'Profiel',
+          condition: UserAccess.hasAccessTo("profiel"),
+          iconclasses : 'fa fa-user',
+          href: '#/lid/profiel'
+        },
+        {
+          label: 'Feedback',
+          condition: UserAccess.hasAccessTo("profiel"),
+          iconclasses : 'fa fa-comments-o',
+          href: 'https://wiki.scoutsengidsenvlaanderen.be/doku.php?id=handleidingen:groepsadmin:betaversie_testen'
+        },
+        {
+          label: 'Oude Groepsadmin',
+          condition: UserAccess.hasAccessTo("profiel"),
+          iconclasses : 'fa fa-institution',
+          href: 'https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/'
+        }
+      ];
 
-    $scope.menuItems = [
-      {
-        label: 'Ledenlijst',
-        condition: UserAccess.hasAccessTo("ledenlijst"),
-        iconclasses : 'fa fa-users',
-        href: '#/ledenlijst'
-      },
-      {
-        label: 'Ledenaantallen',
-        condition: UserAccess.hasAccessToGroepen(),
-        iconclasses : 'fa fa-area-chart',
-        href: '#/orakel'
-      },
-      {
-        label: 'Groepsinstellingen',
-        condition: UserAccess.hasAccessToGroepen(),
-        iconclasses : 'fa fa-cogs',
-        href: '#/groepsinstellingen'
-      },
-      {
-        label: 'Profiel',
-        condition: UserAccess.hasAccessTo("profiel"),
-        iconclasses : 'fa fa-user',
-        href: '#/lid/profiel'
-      },
-      {
-        label: 'Feedback',
-        condition: UserAccess.hasAccessTo("profiel"),
-        iconclasses : 'fa fa-comments-o',
-        href: 'https://wiki.scoutsengidsenvlaanderen.be/doku.php?id=handleidingen:groepsadmin:betaversie_testen'
-      },
-      {
-        label: 'Oude Groepsadmin',
-        condition: UserAccess.hasAccessTo("profiel"),
-        iconclasses : 'fa fa-institution',
-        href: 'https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/'
-      }
-    ];
+    });
+
 
     _.forEach($scope.menuItems, function(menuItem,k) {
-      menuItem.condition.then(function (reponse) {
-        menuItem.condition = reponse;
-
         window.app.positionBody();
-      });
     });
   }
 })();
