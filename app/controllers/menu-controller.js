251436fb04c89eec47702161d78f607aa544a12f
Tinus Van Langendonck <tvl@scoutsengidsenvlaanderen.be>
2018-01-29 14:26:57 +0100

    Menu steller tonen

--- app/controllers/menu-controller.js
+++ app/controllers/menu-controller.js
@@ -5,58 +5,70 @@
     .module('ga.menucontroller', ['ga.services.alert', 'ga.services.dialog', 'ui.bootstrap'])
     .controller('MenuController', MenuController);
 
-  MenuController.$inject = ['$scope', '$timeout', '$window', 'UserAccess'  ];
+  MenuController.$inject = ['$scope', '$timeout', '$window', 'UserAccess'];
 
   function MenuController ($scope, $timeout, $window, UserAccess) {
+
+    function updateMenu(ledenlijst, groep) {
+      $scope.menuItems = [
+        {
+          label: 'Ledenlijst',
+          condition: ledenlijst,
+          iconclasses : 'fa fa-users',
+          href: '#/ledenlijst'
+        },
+        {
+          label: 'Ledenaantallen',
+          condition: groep,
+          iconclasses : 'fa fa-area-chart',
+          href: '#/ledenaantallen'
+        },
+        {
+          label: 'Groepsinstellingen',
+          condition: groep,
+          iconclasses : 'fa fa-cogs',
+          href: '#/groepsinstellingen'
+        },
+        {
+          label: 'Profiel',
+          condition: true,
+          iconclasses : 'fa fa-user',
+          href: '#/lid/profiel'
+        },
+        {
+          label: 'Feedback',
+          condition: true,
+          iconclasses : 'fa fa-comments-o',
+          href: 'https://wiki.scoutsengidsenvlaanderen.be/doku.php?id=handleidingen:groepsadmin:betaversie_testen'
+        },
+        {
+          label: 'Oude Groepsadmin',
+          condition: true,
+          iconclasses : 'fa fa-institution',
+          href: 'https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/'
+        }
+      ];
+
+      $timeout(function() {
+        window.app.positionBody();
+      }, 10);
+    }
+
+    var ledenlijst = false;
+    var groep = false;
+
+    updateMenu(ledenlijst, groep);
+
     UserAccess.hasAccessToGroepen().then(function(conditionGroep){
-      UserAccess.hasAccessTo("ledenlijst").then(function(conditionLedenLijst){
-        UserAccess.hasAccessTo("profiel").then(function(conditionProfiel){
+      ledenlijst |= conditionGroep; // in parktijk komen deze rechten overeen
+      groep |= conditionGroep;
+      updateMenu(ledenlijst, groep);
+    });
 
-          $scope.menuItems = [
-            {
-              label: 'Ledenlijst',
-              condition: conditionLedenLijst,
-              iconclasses : 'fa fa-users',
-              href: '#/ledenlijst'
-            },
-            {
-              label: 'Ledenaantallen',
-              condition: conditionGroep,
-              iconclasses : 'fa fa-area-chart',
-              href: '#/orakel'
-            },
-            {
-              label: 'Groepsinstellingen',
-              condition: conditionGroep,
-              iconclasses : 'fa fa-cogs',
-              href: '#/groepsinstellingen'
-            },
-            {
-              label: 'Profiel',
-              condition: conditionProfiel,
-              iconclasses : 'fa fa-user',
-              href: '#/lid/profiel'
-            },
-            {
-              label: 'Feedback',
-              condition: conditionProfiel,
-              iconclasses : 'fa fa-comments-o',
-              href: 'https://wiki.scoutsengidsenvlaanderen.be/doku.php?id=handleidingen:groepsadmin:betaversie_testen'
-            },
-            {
-              label: 'Oude Groepsadmin',
-              condition: conditionProfiel,
-              iconclasses : 'fa fa-institution',
-              href: 'https://groepsadmin.scoutsengidsenvlaanderen.be/groepsadmin/'
-            }
-          ];
-
-          $timeout(function() {
-            window.app.positionBody();
-          }, 10);
-
-        })
-      });
+    UserAccess.hasAccessTo("ledenlijst").then(function(conditionLedenLijst){
+      ledenlijst |= conditionLedenLijst;
+      groep |= conditionLedenLijst; // in parktijk komen deze rechten overeen
+      updateMenu(ledenlijst, groep);
     });
   }
 })();
