70b42dd54d46c2c23bbb071b05758d93b02077be
jr <jr@scoutsengidsenvlaanderen.be>
2020-09-23 14:53:28 +0200

    T5434: directive om emailvalidatie zelf in beheer te hebben. De ingebouwde email validator controleert niet op een punt in het emailadres

---
+++ app/directives/ga-email.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+  angular
+    .module('ga.lid', [])
+    .directive('validateEmail', function () {
+      var EMAIL_REGEXP = /^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,5})$/;
+      return {
+        require: 'ngModel',
+        restrict: '',
+        link: function (scope, elm, attrs, ctrl) {
+          // only apply the validator if ngModel is present and Angular has added the email validator
+          if (ctrl && ctrl.$validators.email) {
+
+            // this will overwrite the default Angular email validator
+            ctrl.$validators.email = function (modelValue) {
+              return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+            };
+          }
+        }
+      };
+    });
+})();
