d6ffe6a5852eb88bfeb6ece6d7a089ceb9c9112f
jr <jr@scoutsengidsenvlaanderen.be>
2020-09-24 14:12:48 +0200

    T5434: aanpassingen aan de custom email validator

--- app/directives/ga-email.js
+++ app/directives/ga-email.js
@@ -1,22 +1,17 @@
 (function () {
   'use strict';
   angular
-    .module('ga.lid', [])
-    .directive('validateEmail', function () {
+    .module('ga.validatie.email', [])
+    .directive('email', [function () {
       var EMAIL_REGEXP = /^[_a-zA-Z0-9ïéè]+(\.[_a-zA-Z0-9ïéè]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,5})$/;
       return {
+        restrict: 'A',
         require: 'ngModel',
-        restrict: '',
         link: function (scope, elm, attrs, ctrl) {
-          // only apply the validator if ngModel is present and Angular has added the email validator
-          if (ctrl && ctrl.$validators.email) {
-
-            // this will overwrite the default Angular email validator
             ctrl.$validators.email = function (modelValue) {
               return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
             };
-          }
         }
       };
-    });
+    }]);
 })();
