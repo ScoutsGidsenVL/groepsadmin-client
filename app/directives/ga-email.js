01e58356bb493d4e44ebbb63fe6065daab6b5f0f
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2020-10-16 11:51:45 +0200

    T5169: meerdere adressen toelaten in een bcc veld

--- app/directives/ga-email.js
+++ app/directives/ga-email.js
@@ -8,10 +8,37 @@
         restrict: 'A',
         require: 'ngModel',
         link: function (scope, elm, attrs, ctrl) {
-            ctrl.$validators.email = function (modelValue) {
+          ctrl.$validators.email = function (modelValue) {
+            if (!modelValue.includes(";")) {
               return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
-            };
+            } else {
+              var result = true;
+              var emailAdressen = filterAdressen(modelValue);
+              _.each(emailAdressen, function (adres){
+                  if (!EMAIL_REGEXP.test(adres)){
+                    result = false;
+                  }
+              })
+              return result;
+            }
+          };
         }
       };
     }]);
 })();
+
+// Wanneer er meerdere emailadressen worden ingevoerd gaan we deze splitsen
+filterAdressen = function (adressen) {
+  var emailCollection = [];
+  while (adressen.includes(";")) {
+    var index = adressen.indexOf(";");
+    var adres = adressen.substring(0, index);
+    emailCollection.push(adres);
+    adressen = adressen.substring(index + 1, adressen.length)
+  }
+  adres = adressen.substring(0, adressen.length)
+  if (adres.length > 0){
+    emailCollection.push(adres);
+  }
+  return emailCollection;
+}
