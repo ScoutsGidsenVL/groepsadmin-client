7f2e5c0eb1661631dc3f48e929aa33f40c0076c4
Johnny Ressen <jr@scoutsengidsenvlaanderen.be>
2021-02-08 11:26:00 +0100

    T3499: aangevinkte leden toevoegen of verwijderen uit lijst

--- app/services/leden-lijst-service.js
+++ app/services/leden-lijst-service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -12,13 +12,15 @@
 
   function LedenLijstService($log, $q, RestService) {
     var ledenLijstService = {};
+    var ledenLijstVoorVersturen = [];
+    ledenLijstService.alleLedenGeselecteerd = false;
 
     ledenLijstService.loadingLeden = false;
 
-    ledenLijstService.getLeden = function(offset){
+    ledenLijstService.getLeden = function (offset) {
       ledenLijstService.loadingLeden = true;
 
-      return $q(function(resolve,reject){
+      return $q(function (resolve, reject) {
         RestService.Leden.get({offset: offset}).$promise.then(
           function (response) {
             ledenLijstService.loadingLeden = false;
@@ -28,28 +30,86 @@
       })
     }
 
-    ledenLijstService.export = function(type,extension){
+    ledenLijstService.voegLidToeAanMailLijst = function (lid) {
+      ledenLijstVoorVersturen.push(lid);
+    }
+
+    ledenLijstService.verwijderLidUitLijst = function (lid) {
+      var index = -1;
+      for (var i = 0; i < ledenLijstVoorVersturen.length; i++) {
+        if (ledenLijstVoorVersturen[i].id === lid.id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0){
+        ledenLijstVoorVersturen.splice(index, 1);
+      }
+    }
+
+    ledenLijstService.getGeselecteerdeLeden = function () {
+      return ledenLijstVoorVersturen;
+    }
+
+    ledenLijstService.voegAlleLedenToe = function() {
+      ledenLijstService.alleLedenGeselecteerd = !ledenLijstService.alleLedenGeselecteerd;
+    }
+
+    ledenLijstService.wisGeselecteerdeLeden = function () {
+      ledenLijstVoorVersturen = [];
+    }
+
+    ledenLijstService.getAantalGeselecteerdeLeden = function () {
+      return ledenLijstVoorVersturen.length;
+    }
+
+    ledenLijstService.isLidInLijst = function (lid) {
+      var index = -1;
+      for (var i = 0; i < ledenLijstVoorVersturen.length; i++) {
+        if (ledenLijstVoorVersturen[i].id === lid.id) {
+          index = i;
+          break;
+        }
+      }
+      return index >= 0;
+    }
+
+    ledenLijstService.checkAlleLedenGeselecteerd = function () {
+      return ledenLijstService.alleLedenGeselecteerd;
+    }
+
+    ledenLijstService.export = function (type, extension) {
       var deferred = $q.defer();
       var blob, obj = {};
 
-      if(type == 'lijst' && extension == 'csv'){
-        RestService.LedenCsv.get({offset:0}).$promise.then(function(res){
+      var ledenIds = {
+        "lidIds": []
+      }
+
+      if (ledenLijstVoorVersturen.length > 0) {
+        _.forEach(ledenLijstVoorVersturen, function (lid) {
+          ledenIds.lidIds.push(lid.id);
+        })
+      }
+
+      if (type == 'lijst' && extension == 'csv') {
+        RestService.LedenCsv.post({offset: 0}, ledenIds).$promise.then(function (res) {
           blob = new Blob([res.response], {type: 'text/csv'});
           obj.fileUrl = window.URL.createObjectURL(blob);
           obj.title = type + '.' + extension;
           deferred.resolve(obj);
         });
       }
-      if(type == 'lijst' && extension == 'pdf'){
-        RestService.LedenPdf.get({offset:0}).$promise.then(function(res){
+      if (type == 'lijst' && extension == 'pdf') {
+        RestService.LedenPdf.get({offset: 0}).$promise.then(function (res) {
           blob = new Blob([res.response], {type: 'application/pdf'});
           obj.fileUrl = window.URL.createObjectURL(blob);
           obj.title = type + '.' + extension;
           deferred.resolve(obj);
         });
       }
-      if(type == 'steekkaarten' && extension == 'pdf'){
-        RestService.LedenSteekkaarten.get({offset:0}).$promise.then(function(res){
+      if (type == 'steekkaarten' && extension == 'pdf') {
+        RestService.LedenSteekkaarten.get({offset: 0}).$promise.then(function (res) {
           blob = new Blob([res.response], {type: 'application/pdf'});
           obj.fileUrl = window.URL.createObjectURL(blob);
           obj.title = type + '.' + extension;
@@ -61,23 +121,23 @@
     }
 
 
-    ledenLijstService.getNextPrevLid = function(lidId, ledenlijst){
+    ledenLijstService.getNextPrevLid = function (lidId, ledenlijst) {
       var nextLid, prevLid;
 
       // ledenlijst uit rootScope ontdubbelen, op basis van lid.id
       ledenlijst = _.uniqBy(ledenlijst, 'id');
       // zoek index van lidId param
-      var indexLid = _.findIndex(ledenlijst, { 'id': lidId});
+      var indexLid = _.findIndex(ledenlijst, {'id': lidId});
       // neem volgende en vorige lid id uit de ontdubbelde lijst
-      if(ledenlijst[indexLid + 1]){
+      if (ledenlijst[indexLid + 1]) {
         nextLid = ledenlijst[indexLid + 1];
-      }else{
+      } else {
         nextLid = ledenlijst[0];
       }
-      if(ledenlijst[indexLid - 1]){
-        prevLid = ledenlijst[indexLid -1];
-      }else{
-        prevLid = ledenlijst[ledenlijst.length-1];
+      if (ledenlijst[indexLid - 1]) {
+        prevLid = ledenlijst[indexLid - 1];
+      } else {
+        prevLid = ledenlijst[ledenlijst.length - 1];
       }
 
       return [prevLid, nextLid];
@@ -88,4 +148,5 @@
     return ledenLijstService;
   };
 
-})();
+})
+();
